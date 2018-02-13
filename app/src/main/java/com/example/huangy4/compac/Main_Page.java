d82ac22261eb08515ed276e8d8631e5ff6c221f4
Yan Wen Huang <yanwenhuang.95@gmail.com>
2018-02-13 10:23:59 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/huangy4/compac/Main_Page.java
@@ -0,0 +1,13 @@
+package com.example.huangy4.compac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__page);
+    }
+}
