7fc5fb7a55043318ca5248f9934b5e85e7fdaebc
iuhsirod <dorismhui@gmail.com>
2018-02-14 18:58:31 -0500

    Renamed files, main buttons now redirect to new or existing lists, added back button to redirect to main page.

--- app/src/main/java/com/example/huangy4/compac/Main_Page.java
+++ app/src/main/java/com/example/huangy4/compac/Main_Page.java
@@ -3,23 +3,29 @@ package com.example.huangy4.compac;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class Main_Page extends AppCompatActivity {
+    private static final String TAG = "ComPac";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main__page);
+        Log.v(TAG, "Entering Main_Page");
 
-        Button newList = (Button)findViewById(R.id.new_btn);
-        Button existingList = (Button)findViewById(R.id.existing_btn);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        Button newList = (Button)findViewById(R.id.new_list_button);
+        Button existingList = (Button)findViewById(R.id.existing_list_button);
 
         newList.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(Main_Page.this, New_List.class);
+                Log.v(TAG, "New List Button clicked");
+                Intent intent = new Intent(Main_Page.this, New_List_Page.class);
+
                 startActivity(intent);
             }
         });
@@ -27,7 +33,9 @@ public class Main_Page extends AppCompatActivity {
         existingList.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(Main_Page.this, Existing_List.class);
+                Log.v(TAG, "Existing List Button clicked");
+                Intent intent = new Intent(Main_Page.this, Existing_List_Page.class);
+
                 startActivity(intent);
             }
         });
