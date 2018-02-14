2d676d31793a0eaa0225856868b0abbb2d4ad60b
Yan Wen Huang <yanwenhuang.95@gmail.com>
2018-02-13 19:52:27 -0500

    Added buttons and linked to new_list and existing_list activities.

--- app/src/main/java/com/example/huangy4/compac/Main_Page.java
+++ app/src/main/java/com/example/huangy4/compac/Main_Page.java
@@ -1,7 +1,10 @@
 package com.example.huangy4.compac;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main_Page extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class Main_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__page);
+
+        Button newList = (Button)findViewById(R.id.new_btn);
+        Button existingList = (Button)findViewById(R.id.existing_btn);
+
+        newList.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Main_Page.this, New_List.class);
+                startActivity(intent);
+            }
+        });
+
+        existingList.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Main_Page.this, Existing_List.class);
+                startActivity(intent);
+            }
+        });
     }
 }
