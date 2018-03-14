89cc9d13ff43d73ab6999ef42901169d7784f24c
iuhsirod <dorismhui@gmail.com>
2018-03-14 17:23:20 -0400

    attempt at gratata 2.

---
+++ app/src/main/java/com/example/huangy4/compac/Pick_Time_Fragment.java
@@ -0,0 +1,41 @@
+package com.example.huangy4.compac;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class Pick_Time_Fragment extends DialogFragment {
+    private static final String TAG = "ComPac";
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Log.v(TAG, "Entering Pick_Date_Fragment");
+
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), timeSetListener, hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+
+    private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hour, int minute) {
+
+            Log.v(TAG, "Entering onDateSet");
+
+            TextView tv = getActivity().findViewById(getActivity().getCurrentFocus().getId());
+            tv.setText(Integer.toString(hour) + ":" + Integer.toString(minute));
+        }
+    };
+
+}
